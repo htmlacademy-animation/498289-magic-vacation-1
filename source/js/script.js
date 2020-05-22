1f45eab839bd5685dc7c83c8d1d67ce7eb1d8fa6
Marina <vaganova.m.a@gmail.com>
2020-05-22 08:11:14 +0500

    module 1 task 2

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import load from './modules/load.js';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+load();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
